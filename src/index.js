f02d536e5b20ada29f7302b06acac0288f29ef72
Farasat Ali <63093876+farasatAli1@users.noreply.github.com>
2020-12-05 00:41:27 +0500

    Version 1.1 (Added Loader)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Functionality to show Loader when DOM is Loading
+const loader = document.querySelector(".loader");
+const showLoader = () => loader.classList.remove("loader--hide");
+const hideLoader = () => loader.classList.add("loader--hide");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App hideLoader={hideLoader} showLoader={showLoader} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
